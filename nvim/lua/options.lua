387043863728715985a0cb520028546869e570c2
adrivn <adrian.emege@gmail.com>
2024-04-10 00:22:16 +0200

    Added back listchars

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -82,8 +82,8 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 -- WARN: Disabled for now
--- vim.opt.list = true
--- vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.list = true
+vim.opt.listchars = { tab = "│·", trail = "·", nbsp = "␣", extends = "→" }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = "split"
