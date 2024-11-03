5579099ddb397cdc9b306e04e174c6771b71ba4c
am <adrian.emege@gmail.com>
2024-11-03 17:01:29 +0100

    added lsp_signature.nvim

--- nvim/lua/plugins/neat.lua
+++ nvim/lua/plugins/neat.lua
@@ -77,4 +77,17 @@ return {
 		opts = {}, -- for default options, refer to the configuration section for custom setup.
 		cmd = "Trouble",
 	},
+	{
+		"ray-x/lsp_signature.nvim",
+		event = "InsertEnter",
+		opts = {
+			bind = true,
+			handler_opts = {
+				border = "rounded",
+			},
+		},
+		config = function(_, opts)
+			require("lsp_signature").setup(opts)
+		end,
+	},
 }
